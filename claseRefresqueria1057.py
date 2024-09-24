d8a91ad7a30df5aee7012d98503cd85eec383059
Luis Cesar Cervantes <164534073+Luis-Cervantes-1057@users.noreply.github.com>
2024-09-24 12:15:48 -0600

    Add files via upload

--- claseRefresqueria1057.py
+++ claseRefresqueria1057.py
@@ -1,32 +1,32 @@
 # Zona de clase
 class refresqueria:
     #Zona de atributos
-    id_producto = 0
+    id_prod = 0
     nombre = ""
     tipo = ""
-    sabor = ""
-    precio = 0
-    peso = 0
+    sabor =""
+    precio_uni = 0
+    peso = ""
+
 #funciones
     def mostrar(self):
-        print(f"id venta: {ale.id_venta} \nid_empleado: {ale.id_empleado} \nid_cliente: {ale.id_cliente}")
-        print(f"cantidad: {ale.cantidad} \nprecio: {ale.precio} \nfecha: {ale.fecha} \ndireccion: {ale.dire}")
+        print(f"id producto: {ale.id_prod} \nNombre: {ale.nombre} \nTipo: {ale.tipo}")
+        print(f"Sabor: {ale.sabor} \nprecio: {ale.precio_uni} \nPeso {ale.peso}")
     def lista_refe (self):
-        lis_ref=[10,220,123,300,60000,"3 de Marzo del 2024","Calle San miguel 8231-41, Col. Santa Monica"]
+        lis_ref=[10,"Coca-Cola","Refresco","Cola",18,600]
         for x in lis_ref:
             print(x)
     def tupla_ref (self):
-        tup_ref = (10,220,123,300,60000,"3 de Marzo del 2024","Calle San miguel 8231-41, Col. Santa Monica")
+        tup_ref = (10,"Coca-Cola","Refresco","Cola",18,600)
         for x in tup_ref:
             print(x)
     def dicc_ref(self):
         dic_ref = {"id venta: ": "010",
-                    "id empleado:": "220",
-                    "id cliente": 123,
-                    "Cantidad": 300,
-                    "Precio: ": 60000,
-                    "Fecha: ":"3 de Marzo del 2024",
-                    "Direccion: ":"Calle San miguel 8231-41, Col. Santa Monica"}
+                    "nombre: ": "Coca-Cola",
+                    "tipo ": "Refresco",
+                    "Sabor ": "Cola",
+                    "Precio unidad: ": 18,
+                    "peso: ": 600,}
         for x,y in dic_ref.items():
             print(x,y)
     def alta_ref(self):
@@ -37,13 +37,12 @@ class refresqueria:
 ale = refresqueria()
 
 # Usando objetos
-ale.id_venta = 10
-ale.id_empleado = 220
-ale.id_cliente = 123
-ale.cantidad = 300
-ale.precio = 600000
-ale.fecha = "3 de Marzo del 2024"
-ale.dire = "Calle San miguel 8231-41, Col. Santa Monica"
+ale.id_prod = 10
+ale.nombre = "Coca-Cola"
+ale.tipo = "Refresco"
+ale.sabor = "Cola"
+ale.precio_uni = 18
+ale.peso = 600
 
 print("Mostrando datos")
 ale.mostrar()
